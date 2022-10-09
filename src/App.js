3ec317a2eff96bc48d7f15e37be4600778e0a9e3
jeeva3006 <bhuvankarthi23@gmail.com>
2022-10-09 19:07:24 +0530

    trigger time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import GoogleLogin from 'react-google-login';
+
 import './App.css';
 
 function App() {
+  const handleSuccess = () => {
+    console.log("first");
+  };
+
+  const handleFailure = () => {
+    console.log("first");
+  };
+
+  const config = {
+    clientId: '471225452171-2ss2d4v4r91paf76r1t2osv8urvpu8vs.apps.googleusercontent.com',
+    clientSecretKey: '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'
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hi
+
+      <GoogleLogin
+        // clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        clientId={config.clientId}
+        buttonText="Login"
+        onSuccess={() => handleSuccess()}
+        onFailure={() => handleFailure()}
+        cookiePolicy={'single_host_origin'}
+      />
+
+      {/* <GoogleLogin
+        clientId={config.clientId}
+        buttonText="Login"
+        onSuccess={() => handleSuccess()}
+        onFailure={() => handleFailure()}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+      /> */}
     </div>
   );
 }
