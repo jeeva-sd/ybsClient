224fe5cd543f0ac56733ea58bb1ae58c0d1b0897
jeeva3006 <bhuvankarthi23@gmail.com>
2022-10-30 02:15:00 +0530

    host changes.

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -4,5 +4,5 @@ export const blogId = '8980519991408762151';
 export const apiKey = 'AIzaSyDrSnpzHVBsETvCZRPsFJLx118xJYh36UE';
 export const token = 'ya29.a0Aa4xrXMacDGaKDtEmd9A2FZRCsNHuMOhHJ6Wj4_-nIY4b9VA6xZSavX_uIqnkNoii_AsHPOu43ovr2Q8wD63paRPqgTzVKUKLbGSZXsOjoDinZOD-braY6e4sxGELo8jDvtcI2vWFDSa25-G0Tqa8AnpjaBJaCgYKATASARMSFQEjDvL9elQxBlAfo2U7UV4mGf4KZA0163';
 
-
-export const host = 'http://localhost:1650';
+// export const host = 'http://localhost:1650';
+export const host = 'https://ybsms.herokuapp.com';
